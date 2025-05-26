023208485a4b5c685756faf608caf32527773d07
biribillidev <biribillijoaovitor@gmail.com>
2025-05-25 22:14:15 -0300

    função criar humanos adicionada

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ let vidaGorila = 100;
 let humanosRestantes = 100;
 let humanos = [];
 const logDiv = document.getElementById("log");
+const humanosContainer = document.getElementById("humanos");
+const somAtaque = new Audio("assets/som-ataque.mp3");
+
+function criarHumanos() {
+  for (let i = 0; i < 100; i++) {
+    const h = document.createElement("img");
+    h.src = "assets/humano.png";
+    h.classList.add("humano", "vivo");
+    humanos.push(h);
+    humanosContainer.appendChild(h);
+  }
+}
+
+
